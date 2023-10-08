21b938a90a42b4dfc6c4dd96749f1dd02aeb0360
JLuisNG <127897120+JLuisNG@users.noreply.github.com>
2023-10-07 21:04:19 -0400

    COMMIT - 9.4

---
+++ js/02-lightbox.js
@@ -0,0 +1,27 @@
+import { galeriaItems } from "./galeria-items.js";
+// Change code below this line
+const galeria = document.querySelector(".galeria");
+
+const createListItem = image => {
+  return `<li class="galeria__item">
+    <a class="galeria__link" href="${image.original}">
+        <img class="galeria__image" src="${image.preview}" alt="${image.description}" />
+    </a>
+</li>`;
+};
+
+for (const image of galeriaItems) {
+  const newImage = createListItem(image);
+  galeria.innerHTML += newImage;
+}
+
+var lightbox = new SimpleLightbox(".galeria a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  captionPosition: "bottom",
+});
+
+lightbox.on("show.simplelightbox", function (e) {
+  e.preventDefault();
+  // console.log(e.target);
+});
